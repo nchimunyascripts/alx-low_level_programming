fe48b86f8ea2648997b5082b9f7e479453af30bc
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2022-10-14 00:37:40 +0200

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(a));
 	printf("Size of an int: %ld bytes(s)\n", sizeof(b));
-	printf("Size of a long int: %ld bytes(s)\n", sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
