779376d1ad8d58d1e301a82e88dde0ba120bbf78
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2022-10-21 00:53:19 +0200

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 		}
-		i++;
 	}
 	_putchar('\n');
 }
