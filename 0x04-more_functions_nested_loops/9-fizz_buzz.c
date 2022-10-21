1cd377df36439731fc37f16bcd221955754c0ac0
joseph-mathias <nchimunya882@gmail.com>
2022-10-21 15:48:39 +0200

    fizzy

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,34 @@
 
 int main(void)
 {
-	int i;
+    int i = 1;
 
-	if ((i % 3) == 0 && (i % 5) != 0)
-		printf(" Fizz");
-	else if ((i % 5) == 0 && (i % 3) != 0)
-		printf(" Buzz");
-	else if ((i % 3) == 0 && (i % 5) == 0)
+    while (i < 101)
+    {
+	if (( % 3 == 0 && i % 5 == 0)
+	{
 		printf(" FizzBuzz");
-	else if (i == 1)
-		printf("%i", i);
+	}
+	else if (i % 3 == 0)
+	{
+		printf(" Fuzz");
+	}
+	else if (i % 5 == 0)
+	{
+		printf(" Buzz");
+	}
 	else
-		printf("%d", i);
-	printf("\n");
+	{
+	    printf("%d", i);
+	}
+	
+	if (i != 100)
+	{
+	    printf(" ");
+	}
+	i++;
+    }
+    printf("\n");
 
-	return (0);
+    return (0);
 }
