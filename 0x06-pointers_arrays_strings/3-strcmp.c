d344c45021c5961fecd9631000c039403d577451
joseph-mathias <nchimunya882@gmail.com>
2022-10-26 13:17:09 +0200

    fix

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: variable string one
+ * @s2: varibale strings two
+ * Return: 0 or 1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+    
+}
