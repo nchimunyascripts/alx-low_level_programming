55559a9d3842c7c63219a7edb72c39ddbeb1ff01
joseph-mathias <nchimunya882@gmail.com>
2022-10-28 11:48:03 +0200

    up

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: variable
+ * @n2: variable
+ * @r: the buffer for the function
+ * @size_r: buffer size
+ * Return: 
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+}
