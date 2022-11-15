25a9db0540874ff07e997a2efd01297e17d57fe8
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2022-11-15 02:41:39 +0200

    hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,7 +6,9 @@
  * init_dog - a function that initialize a Structure
  * @d : our Structure@name : the name we are going to attribute
  * @age : the age we are going to attribute
+ * @name: variable
  * @owner : the owner name we are going to attribute
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,15 +3,16 @@
 
 /**
  * free_dog - free dogs
+ * @d: dog struct
  * Return: void
  */
 
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
-		return;
-
-	free(d->name);
-	free(d->owner);
-	free(d);
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
