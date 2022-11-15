a8e46ed509a56a2dbc3132e0efeb52331bfb6cb9
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2022-11-15 03:46:31 +0200

    hee

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,7 +5,8 @@
  * struct - dog data type
  * @name: string pointer variable
  * @age: variable
- * @ower: variable
+ * @owner: variable
+ * Description: name, age and owner of a dog
  */
 
 typedef struct dog
@@ -13,10 +14,11 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+} dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
