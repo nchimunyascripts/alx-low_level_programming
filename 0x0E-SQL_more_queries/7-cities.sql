28fa34eedffe94db4f1fa69a5e4ecb0525be5fe5
nhcimunyascripts <nchimunya.backlogcode@gmail.com>
2024-03-13 23:38:04 +0200

    folder removed

--- 0x0E-SQL_more_queries/7-cities.sql
+++
@@ -1,6 +0,0 @@
--- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL serve
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT AUTO_INCREMENT PRIMARy KEY, 
-  state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
-  name VARCHAR(256) NOT NULL);
-
